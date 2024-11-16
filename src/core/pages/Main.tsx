619a97c7ba974cad6be2fd6854502a7772a48711
Maciej <maciek333programowanie@gmail.com>
2024-11-16 17:03:59 +0100

    add base for rich text formatter

--- src/core/pages/Main.tsx
+++ src/core/pages/Main.tsx
@@ -1,13 +1,16 @@
 // import ExampleAnimation from "../components/function-components/Animation/_EXAMPLE/ExampleAnimation";
 // import ExampleDragDrop from "../components/function-components/DragDrop/_EXAMPLE/ExampleDragDrop";
-import ExampleDragDrop2 from "../components/function-components/DragDrop2/_EXAMPLE/ExampleDragDrop2";
+// import ExampleDragDrop2 from "../components/function-components/DragDrop2/_EXAMPLE/ExampleDragDrop2";
+
+import RichTextEditor from "../components/function-components/form-elements/RichTextEditor";
 
 function Main() {
   return (
     <div>
       {/* <ExampleAnimation /> */}
       {/* <ExampleDragDrop /> */}
-      <ExampleDragDrop2 />
+      {/* <ExampleDragDrop2 /> */}
+      <RichTextEditor />
     </div>
   );
 }
